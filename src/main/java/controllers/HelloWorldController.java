74e033e14312213325d63ecd0bee69473e50ee35
ASkeTizy <artemnevmerz@gmail.com>
2025-11-19 12:31:11 +0300

    project start commit

---
+++ src/main/java/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return model.getAttribute("message").toString();
+    }
+}
